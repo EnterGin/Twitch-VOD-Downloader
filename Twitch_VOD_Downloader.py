4bfe899ae5c62f9565d5476e189f5331deb6110e
EnterGin <51099894+EnterGin@users.noreply.github.com>
2020-07-12 16:03:09 +0300

    Linux compatibility

--- Twitch_VOD_Downloader.py
+++ Twitch_VOD_Downloader.py
@@ -1,10 +1,11 @@
-#Twitch VOD Downloader v1.2.0 https://github.com/EnterGin/Twitch-VOD-Downloader
+# Twitch VOD Downloader v1.3.0 https://github.com/EnterGin/Twitch-VOD-Downloader
 
 import requests
 import os
 import time
 import json
 import sys
+import platform
 import subprocess
 import datetime
 from datetime import timedelta
@@ -15,16 +16,18 @@ class TwitchDownloader:
     def __init__(self):
         # global configuration
         self.client_id        = "kimne78kx3ncx6brgo4mv6wki5h1ko" # Don't change this
-        self.ffmpeg_path      = r"D:\\twitch"                    # path to ffmpeg.exe
+        self.ffmpeg_path      = r"D:\\twitch"                    # path to ffmpeg.exe. Leave blank if Linux or ffmpeg in env PATH
         self.root_path        = r"D:\\twitch"                    # path to recorded and processed streams
         self.timezoneName     = 'Europe/Moscow'                  # name of timezone (list of timezones: https://stackoverflow.com/questions/13866926/is-there-a-list-of-pytz-timezones)
         self.chatdownload     = 1                                # 0 - disable chat downloading, 1 - enable chat downloading
-        self.cmdstate         = 1                                # 0 - not minimazed cmd close after processing, 1 - minimazed cmd close after processing, 2 - minimazed cmd don't close after processing
+        self.cmdstate         = 1                                # Windows: 0 - not minimazed cmd close after processing, 1 - minimazed cmd close after processing, 2 - minimazed cmd don't close after processing, 3 - no terminal, do in background
+                                                                 # Linux:   0 - not minimazed terminal close after processing, 1 - not minimazed terminal don't close after processing, 2 - no terminal, do in background
         self.vod_folder       = 1                                # 0 - not create folder for processed VOD, 1 - create folder for processed VOD
         self.short_folder     = 0                                # 0 - only date in processed VOD folder, 1 - date, title, game and username in processed VOD folder
         self.deletevod        = 1                                # 0 - not delete downloaded VOD file, 1 - delete downloaded VOD file
         self.hls_segments     = 10                               # 1-10 it's possible to use multiple threads to potentially increase the throughput
         self.streamlink_debug = 0                                # 0 - don't show streamlink debug, 1 - show streamlink debug
+        self.warning_windows  = 1                                # 0 - don't show warning windows (warnings will only be printed in terminal), 1 - show warning windows
         
         # vod configuration
         self.vod_id  = ""
@@ -40,35 +43,70 @@ class TwitchDownloader:
         except requests.exceptions.RequestException as e:
             if e.response != None:
                 if e.response.reason == 'Bad Request':
-                    vod_not_valid_window = "cmd.exe /c start".split()
-                    subprocess.call(vod_not_valid_window + ['echo', 'VOD ID is not a valid ID.'])
+                    if self.warning_windows == 1:
+                        if self.osCheck == 0:
+                            subprocess.call(self.main_cmd_window + ['echo', 'VOD ID is not a valid ID.'])
+                        else:
+                            subprocess.call(' '.join(self.main_cmd_window + ['bash', '-c', "'" + 'echo', '"VOD ID is not a valid ID."; exec bash' + "'"]), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                    print("VOD ID is not a valid ID.")
                     sys.exit()
                 elif e.response.reason == 'Not Found':
-                    vod_not_found_window = "cmd.exe /c start".split()
-                    subprocess.call(vod_not_found_window + ['echo', 'VOD not found.'])
+                    if self.warning_windows == 1:
+                        if self.osCheck == 0:
+                            subprocess.call(self.main_cmd_window + ['echo', 'VOD not found.'])
+                        else:
+                            subprocess.call(' '.join(self.main_cmd_window + ['bash', '-c', "'" + 'echo', '"VOD not found."; exec bash' + "'"]), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                    print("VOD not found.")
                     sys.exit()
                 else:
                     vod_response_another_error        = str(e.response.reason)
-                    vod_response_another_error_window = "cmd.exe /c start".split()
-                    subprocess.call(vod_response_another_error_window + ['echo', vod_response_another_error])
+                    if self.warning_windows == 1:
+                        if self.osCheck == 0:
+                            subprocess.call(self.main_cmd_window + ['echo', vod_response_another_error])
+                        else:
+                            subprocess.call(' '.join(self.main_cmd_window + ['bash', '-c', "'" + 'echo', '"' + vod_response_another_error + '"; exec bash' + "'"]), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                    print(vod_response_another_error)
                     sys.exit()
             else:
                 vod_another_error        = str(e)
-                vod_another_error_window = "cmd.exe /c start".split()
-                subprocess.call(vod_another_error_window + ['echo', vod_another_error])
+                if self.warning_windows == 1:
+                    if self.osCheck == 0:
+                        subprocess.call(self.main_cmd_window + ['echo', vod_another_error])
+                    else:
+                        subprocess.call(' '.join(self.main_cmd_window + ['bash', '-c', "'" + 'echo', '"' + vod_another_error + '"; exec bash' + "'"]), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                print(vod_another_error)
                 sys.exit()
 
         self.username = vod_infodic["channel"]["name"]
         return vod_infodic
 
     def run(self):
-        # cmdstatecommand
-        if self.cmdstate == 2:
-            self.cmdstatecommand = "/min cmd.exe /k".split()
-        elif self.cmdstate == 1:
-            self.cmdstatecommand = "/min".split()
+        # detect os
+        if sys.platform.startswith('win32'):
+            self.osCheck = 0
+        elif sys.platform.startswith('linux'):
+            self.osCheck = 1
+            if self.cmdstate == 3:
+                self.cmdstate = 2
         else:
-            self.cmdstatecommand = "".split()
+            print('Your OS might not be supported.\n')
+            return
+
+        # cmdstatecommand
+        if self.osCheck == 0:
+            if self.cmdstate == 2:
+                self.cmdstatecommand = "/min cmd.exe /k".split()
+            elif self.cmdstate == 1:
+                self.cmdstatecommand = "/min".split()
+            else:
+                self.cmdstatecommand = "".split()
+            self.main_cmd_window = "cmd.exe /c start".split()
+        else:
+            if self.cmdstate == 1:
+                self.linuxstatecomma = "; exec bash'"
+            elif self.cmdstate == 0:
+                self.linuxstatecomma = "'"
+            self.main_cmd_window = "gnome-terminal --".split()
 
         # streamlink debug
         if self.streamlink_debug == 1:
@@ -77,8 +115,9 @@ class TwitchDownloader:
             self.debug_cmd = "".split()
 
         # start text
-        print('Twitch VOD Downloader v1.2.0')
+        print('Twitch VOD Downloader v1.3.0')
         print('Configuration:')
+        print('OS: ' + "Windows " + platform.release() if self.osCheck == 0 else 'OS: ' + "Linux " + platform.release())
         print('Root path: ' + self.root_path)
         print('Ffmpeg path: ' + self.ffmpeg_path)
         self.timezone = pytz.timezone(self.timezoneName).localize(datetime.datetime.now()).tzinfo._utcoffset.seconds/60/60
@@ -133,11 +172,14 @@ class TwitchDownloader:
         recorded_filename = os.path.join(self.downloaded_path, stream_filename)
 
         if len(recorded_filename) >= 260:
-            long_title_window = "cmd.exe /c start".split()
-
             difference = len(stream_title) - len(recorded_filename) + 250
             if difference < 0:
-                subprocess.call(long_title_window + ['echo', 'Path to stream is too long. (Max path length is 259 symbols) Title cannot be cropped, please check root path.'])
+                if self.warning_windows == 1:
+                    if self.osCheck == 0:
+                        subprocess.call(self.main_cmd_window + ['echo', 'Path to stream is too long. (Max path length is 259 symbols) Title cannot be cropped, please check root path.'])
+                    else:
+                        subprocess.call(' '.join(self.main_cmd_window + ['bash', '-c', "'" + 'echo', '"Path to stream is too long. (Max path length is 259 symbols) Title cannot be cropped, please check root path."; exec bash' + "'"]), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                print("Path to stream is too long. (Max path length is 259 symbols) Title cannot be cropped, please check root path.")
                 sys.exit()  
             else:
                 stream_title = stream_title[:difference]
@@ -154,15 +196,18 @@ class TwitchDownloader:
         processed_filename = os.path.join(processed_stream_path, stream_filename)
 
         if len(processed_filename) >= 260:
-            long_title_window = "cmd.exe /c start".split()
-
             if self.short_folder == 1 or self.vod_folder == 0:
                 difference = len(stream_title) - len(processed_filename) + 250
             else:
                 difference = int((2*len(stream_title) - len(processed_filename) + 250)/2)
 
             if difference < 0:
-                subprocess.call(long_title_window + ['echo', 'Path to stream is too long. (Max path length is 259 symbols) Title cannot be cropped, please check root path.'])
+                if self.warning_windows == 1:
+                    if self.osCheck == 0:
+                        subprocess.call(self.main_cmd_window + ['echo', 'Path to stream is too long. (Max path length is 259 symbols) Title cannot be cropped, please check root path.'])
+                    else:
+                        subprocess.call(' '.join(self.main_cmd_window + ['bash', '-c', "'" + 'echo', '"Path to stream is too long. (Max path length is 259 symbols) Title cannot be cropped, please check root path."; exec bash' + "'"]), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                print("Path to stream is too long. (Max path length is 259 symbols) Title cannot be cropped, please check root path.")
                 sys.exit()  
             else:
                 stream_title = stream_title[:difference]
@@ -179,28 +224,47 @@ class TwitchDownloader:
                 else:
                     processed_stream_path = self.processed_path
                 processed_filename = os.path.join(processed_stream_path, stream_filename)
-                subprocess.call(long_title_window + ['echo', 'Path to stream is too long. (Max path length is 259 symbols) Title will be cropped, please check root path.'])
+                if self.warning_windows == 1:
+                    if self.osCheck == 0:
+                        subprocess.call(self.main_cmd_window + ['echo', 'Path to stream is too long. (Max path length is 259 symbols) Title will be cropped, please check root path.'])
+                    else:
+                        subprocess.call(' '.join(self.main_cmd_window + ['bash', '-c', "'" + 'echo', '"Path to stream is too long. (Max path length is 259 symbols) Title will be cropped, please check root path."; exec bash' + "'"]), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                print("Path to stream is too long. (Max path length is 259 symbols) Title will be cropped, please check root path.")
 
         if(os.path.isdir(processed_stream_path) is False):
             os.makedirs(processed_stream_path)
 
         if self.chatdownload == 1:
-            subtitles_window = "cmd.exe /c start".split() + self.cmdstatecommand
-            subprocess.call(subtitles_window + ["tcd", "-v", self.vod_id, "--timezone", self.timezoneName, "-f", "irc,ssa,json", "-o", processed_stream_path])
+            if self.osCheck == 0:
+                subtitles_window = self.main_cmd_window + self.cmdstatecommand
+                if self.cmdstate == 3:
+                    subprocess.Popen(['tcd', "-v", self.vod_id, "--timezone", self.timezoneName, "-f", "irc,ssa,json", "-o", processed_stream_path], stdout=None, stderr=None)
+                else:
+                    subprocess.call(subtitles_window + ['tcd', "-v", self.vod_id, "--timezone", self.timezoneName, "-f", "irc,ssa,json", "-o", processed_stream_path])
+            else:
+                if self.cmdstate == 2:
+                    subprocess.Popen(['tcd', "-v", self.vod_id, "--timezone", self.timezoneName, "-f", "irc,ssa,json", "-o", processed_stream_path], stdout=None, stderr=None)
+                else:
+                    subprocess.call(' '.join(self.main_cmd_window + ['bash', '-c', "'tcd", "-v", self.vod_id, "--timezone", self.timezoneName, "-f", "irc,ssa,json", "-o", '"' + processed_stream_path + '"' + self.linuxstatecomma]), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
         subprocess.call(["streamlink", "--hls-segment-threads", str(self.hls_segments), "twitch.tv/videos/" + self.vod_id, self.quality] + self.debug_cmd + ["-o", recorded_filename])
 
         print("Downloading stream is done. Fixing video file.")
         if(os.path.exists(recorded_filename) is True):
             try:
-                os.chdir(self.ffmpeg_path)
+                if self.ffmpeg_path != "" and self.osCheck == 0:
+                    os.chdir(self.ffmpeg_path)
                 subprocess.call(['ffmpeg', '-y', '-i', recorded_filename, '-analyzeduration', '2147483647', '-probesize', '2147483647', '-c:v', 'copy', '-start_at_zero', '-copyts', '-bsf:a', 'aac_adtstoasc', processed_filename])
                 if(os.path.exists(processed_filename) is True):
                     if self.deletevod == 1:
                         os.remove(recorded_filename)
                 else:
-                    processing_err_window = "cmd.exe /c start".split()
-                    subprocess.call(processing_err_window + ['echo', 'Processed file not found. VOD will not be deleted.'])
+                    if self.warning_windows == 1:
+                        if self.osCheck == 0:
+                            subprocess.call(self.main_cmd_window + ['echo', 'Processed file not found. VOD will not be deleted.'])
+                        else:
+                            subprocess.call(' '.join(self.main_cmd_window + ['bash', '-c', "'" + 'echo', '"Processed file not found. VOD will not be deleted."; exec bash' + "'"]), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                    print("Processed file not found. VOD will not be deleted.")
             except Exception as e:
                 print(e)
         else:
